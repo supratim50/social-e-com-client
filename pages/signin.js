6e7f55356e2b29a15c7d495ebe0e23da85ae7f93
supratim50 <supratimmondal50@gmail.com>
2021-06-12 18:49:38 +0530

    add navbar empty component

--- pages/signin.js
+++ pages/signin.js
@@ -2,9 +2,12 @@ import Input from "../components/Input/Input";
 
 const signin = () => {
   return (
-    <div>
-      <Input placeholder="Name" />
-    </div>
+    <section className={`d-flex`}>
+      <h1>hello</h1>
+      <h1>world</h1>
+      <h1>hey</h1>
+      <h1>hello</h1>
+    </section>
   );
 };
 
